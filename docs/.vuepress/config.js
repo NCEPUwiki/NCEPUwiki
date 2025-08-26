41c2ff0d2e5c1ac7c0f6d62d227d26dd98e42bd0
TakakuraMako <1361942776@qq.com>
2025-08-26 21:52:34 +0800

    删除latex

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,8 +13,8 @@ module.exports = {
     ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
     ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
-    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css' }],
-    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/markdown-it-texmath@0.8.0/css/texmath.css' }],
+    // ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css' }],
+    // ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/markdown-it-texmath@0.8.0/css/texmath.css' }],
     ['meta', { name: 'baidu-site-verification', content: 'codeva-OLOYnzsQjW' }],
     ['meta', { name: 'msvalidate.01', content: '0D95252B3D48510049083243CB8379A8' }],
   ],
