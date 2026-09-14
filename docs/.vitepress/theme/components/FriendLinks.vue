2aa4a134e926c5b7a0314cf0fe650061c82b7f02
TakakuraMako <1361942776@qq.com>
2026-09-14 11:43:20 +0800

    链接更新

--- docs/.vitepress/theme/components/FriendLinks.vue
+++ docs/.vitepress/theme/components/FriendLinks.vue
@@ -2,11 +2,11 @@
 import HoverMedia from './HoverMedia.vue'
 
 const links = [
-	{ name: '落点华电有趣青年', image: '/img/10/10/落点华电.jpg', url: 'http://weixin.qq.com/r/mp/whN7YxDEaE0XrVQA90bQ', description: '华电最全生活指南 · 资讯吐槽 · 校园故事，非官方自媒体', tag: '校园媒体', qr: true },
-	{ name: '保定表白墙微信', image: '/img/10/10/保定表白墙微信头像.jpg', url: '/img/10/10/保定表白墙微信.jpg', description: '非官方的华电校园自媒体平台，为华电学子服务', tag: '校园社区', preview: true },
-	{ name: '保定表白墙 QQ', image: '/img/10/10/保定表白墙QQ头像.jpg', url: '/img/10/10/保定表白墙QQ.jpg', description: '学长学姐 2018 年创立至今，二手 / 树洞 / 拼车 / 答疑，为爱情服务', tag: '校园社区', preview: true },
-	{ name: '柊三米', image: '/img/10/10/柊三米.webp', url: 'https://space.bilibili.com/163004797', description: '一位学长，制作了新生入学指南发布在 B 站', tag: '同学创作' },
-	{ name: 'SWJTUwiki', image: '/img/10/10/西南交通大学校徽.png', url: 'https://wiki.swjtu.top/', description: '西南交通大学 Wiki', tag: '兄弟院校' },
+	{ name: '落点华电有趣青年', image: 'https://img.ncepuinfo.cc/10/10/落点华电.jpg', url: 'http://weixin.qq.com/r/mp/whN7YxDEaE0XrVQA90bQ', description: '华电最全生活指南 · 资讯吐槽 · 校园故事，非官方自媒体', tag: '校园媒体', qr: true },
+	{ name: '保定表白墙微信', image: 'https://img.ncepuinfo.cc/10/10/保定表白墙微信头像.jpg', url: 'https://img.ncepuinfo.cc/10/10/保定表白墙微信.jpg', description: '非官方的华电校园自媒体平台，为华电学子服务', tag: '校园社区', preview: true },
+	{ name: '保定表白墙 QQ', image: 'https://img.ncepuinfo.cc/10/10/保定表白墙QQ头像.jpg', url: 'https://img.ncepuinfo.cc/10/10/保定表白墙QQ.jpg', description: '学长学姐 2018 年创立至今，二手 / 树洞 / 拼车 / 答疑，为爱情服务', tag: '校园社区', preview: true },
+	{ name: '柊三米', image: 'https://img.ncepuinfo.cc/10/10/柊三米.webp', url: 'https://space.bilibili.com/163004797', description: '一位学长，制作了新生入学指南发布在 B 站', tag: '同学创作' },
+	{ name: 'SWJTUwiki', image: 'https://img.ncepuinfo.cc/10/10/西南交通大学校徽.png', url: 'https://wiki.swjtu.top/', description: '西南交通大学 Wiki', tag: '兄弟院校' },
 	{ name: '西邮 Wiki', image: 'https://p.qlogo.cn/gh/924632788/924632788/100/', url: 'https://wiki.cooo.site/', description: '西安邮电大学校园知识库', tag: '兄弟院校' },
 ]
 </script>
